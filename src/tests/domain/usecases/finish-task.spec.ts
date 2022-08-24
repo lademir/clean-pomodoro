7a71bc30172559d73f299d42bf937663c7cced13
lademir <lademirjunior@hotmail.com>
2022-08-24 11:08:37 -0300

    ensure FinishTask throws if userId is invalid

--- src/tests/domain/usecases/finish-task.spec.ts
+++ src/tests/domain/usecases/finish-task.spec.ts
@@ -3,21 +3,46 @@ class FinishTask{
     constructor(private readonly repo: LoadFinishTaskRepository) {}
 
     async perform({id, userId}: {id: string, userId: string}): Promise<void> {
-        const task = await this.repo.loadTask({id, userId})
-        if(task === undefined) throw new Error()
+        const taskGroup = await this.repo.loadTask({id, userId})
+        if(taskGroup === undefined) throw new Error()
+        if(taskGroup.tasks.some(task => task.userId !== userId)) throw new Error()
     }
 }
 
 interface LoadFinishTaskRepository{
-    loadTask(input: {id: string, userId: string}): Promise<any>
+    loadTask(input: {id: string, userId: string}): Promise<TaskGroup | undefined>
+}
+
+type TaskGroup = {
+    tasks: Task[]
+}
+
+type Task = {
+    id: string
+    userId: string
+    description: string
+    finishedAt: Date
+    title: string
+    status: string
 }
 
 class LoadFinishTaskRepositoryMock implements LoadFinishTaskRepository{
     taskId?: string
     callscount = 0
-    output: any = 'any_value'
+    output?: TaskGroup = {
+        tasks: [
+            {
+                id: 'any_id',
+                userId: 'any_user_id',
+                description: 'any_description',
+                finishedAt: new Date(),
+                title: 'any_title',
+                status: 'any_status'
+            }
+        ]
+    }
 
-    async loadTask({id}: {id: string, userId: string}): Promise<any> {
+    async loadTask({id}: {id: string, userId: string}): Promise<TaskGroup | undefined> {
         this.taskId = id
         this.callscount++
         return this.output
@@ -41,6 +66,11 @@ const makeSut = (): SutTypes => {
 describe('FinishTask', () => {
     const id = 'any_task_id'
     const userId = 'any_user_id'
+    const description = 'any_description'
+    const finishedAt = new Date()
+    const title = 'any_title'
+    const status = 'any_status'
+
     it('should get task with correct id', async () => {
         const { sut, loadFinishTaskRepository } = makeSut()
     
@@ -55,6 +85,25 @@ describe('FinishTask', () => {
     
         const promise =  sut.perform({id, userId})
     
-        expect(promise).rejects.toThrow()
+        expect(promise).rejects.toThrowError()
+    });
+    it('should return throw if userId is invalid', async () => {
+        const { sut, loadFinishTaskRepository } = makeSut()
+        loadFinishTaskRepository.output = {
+            tasks: [
+                {
+                    id,
+                    userId,
+                    description,
+                    finishedAt,
+                    title,
+                    status
+                }
+            ]
+        }
+    
+        const promise =  sut.perform({id, userId: 'invalid_user_id'})
+    
+        expect(promise).rejects.toThrowError()
     });
 });
