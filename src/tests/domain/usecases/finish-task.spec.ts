2c42885086f386eb11e9b2d1f020b6c41b323645
lademir <lademirjunior@hotmail.com>
2022-08-24 10:46:22 -0300

    ensure FinishTask throws if taskId is invalid

--- src/tests/domain/usecases/finish-task.spec.ts
+++ src/tests/domain/usecases/finish-task.spec.ts
@@ -1,33 +1,60 @@
-
+export{}
 class FinishTask{
     constructor(private readonly repo: LoadFinishTaskRepository) {}
 
-    async perform(taskId: string): Promise<void> {
-        this.repo.loadTask(taskId)
+    async perform({id, userId}: {id: string, userId: string}): Promise<void> {
+        const task = await this.repo.loadTask({id, userId})
+        if(task === undefined) throw new Error()
     }
 }
 
 interface LoadFinishTaskRepository{
-    loadTask(taskId: string): Promise<void>
+    loadTask(input: {id: string, userId: string}): Promise<any>
 }
 
 class LoadFinishTaskRepositoryMock implements LoadFinishTaskRepository{
     taskId?: string
+    callscount = 0
+    output: any = 'any_value'
 
-    async loadTask(taskId: string): Promise<void> {
-        this.taskId = taskId
+    async loadTask({id}: {id: string, userId: string}): Promise<any> {
+        this.taskId = id
+        this.callscount++
+        return this.output
+    }
+}
+
+type SutTypes = {
+    sut: FinishTask
+    loadFinishTaskRepository: LoadFinishTaskRepositoryMock
+}
+
+const makeSut = (): SutTypes => {
+    const loadFinishTaskRepository = new LoadFinishTaskRepositoryMock()
+    const sut = new FinishTask(loadFinishTaskRepository)
+    return {
+        sut,
+        loadFinishTaskRepository
     }
 }
 
 describe('FinishTask', () => {
-    const taskId = 'any_task_id'
-    const description = 'any_description'
-    it('should load task with correct ID', async () => {
-        const loadFinishTaskRepository = new LoadFinishTaskRepositoryMock()
-        const sut = new FinishTask(loadFinishTaskRepository)
-
-        await sut.perform(taskId)
-
-        expect(loadFinishTaskRepository.taskId).toBe(taskId)
+    const id = 'any_task_id'
+    const userId = 'any_user_id'
+    it('should get task with correct id', async () => {
+        const { sut, loadFinishTaskRepository } = makeSut()
+    
+        await sut.perform({id, userId})
+    
+        expect(loadFinishTaskRepository.taskId).toBe(id)
+        expect(loadFinishTaskRepository.callscount).toBe(1)
+    });
+    it('should return throw if taskId is invalid', async () => {
+        const { sut, loadFinishTaskRepository } = makeSut()
+        loadFinishTaskRepository.output = undefined
+    
+        const promise =  sut.perform({id, userId})
+    
+        expect(promise).rejects.toThrow()
     });
 });
