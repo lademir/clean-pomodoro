d89d75de1e2681061248e7a3b8fc57671a3c62d2
lademir <lademirjunior@hotmail.com>
2022-08-25 13:18:35 -0300

    ensure FinishTask throw if task is already done

--- src/tests/domain/usecases/finish-task.spec.ts
+++ src/tests/domain/usecases/finish-task.spec.ts
@@ -2,20 +2,19 @@ export{}
 class FinishTask{
     constructor(private readonly repo: LoadFinishTaskRepository) {}
 
-    async perform({id, userId}: {id: string, userId: string}): Promise<void> {
-        const taskGroup = await this.repo.loadTask({id, userId})
-        if(taskGroup === undefined) throw new TaskIdInvalidError()
-        if(taskGroup.tasks.some(task => task.userId !== userId)) throw new UserIdInvalidError()
+    async perform({ id, userId }: {id: string, userId: string}): Promise<void> {
+        const task = await this.repo.loadTask({id, userId})
+        if(task === undefined) throw new TaskIdInvalidError()
+        if(task.userId !== userId) throw new UserIdInvalidError()
+        if(task.status === "done") throw new TaskAlreadyDoneError()
+
     }
 }
 
 interface LoadFinishTaskRepository{
-    loadTask(input: {id: string, userId: string}): Promise<TaskGroup | undefined>
+    loadTask(input: { id: string, userId: string }): Promise<Task | undefined>
 }
 
-type TaskGroup = {
-    tasks: Task[]
-}
 
 type Task = {
     id: string
@@ -23,8 +22,10 @@ type Task = {
     description: string
     finishedAt: Date
     title: string
-    status: string
+    status: TaskStatus
 }
+
+type TaskStatus = "done" | "pending"
 
 class TaskIdInvalidError extends Error {
     constructor() {
@@ -40,23 +41,29 @@ class UserIdInvalidError extends Error {
     }
 }
 
+class TaskAlreadyDoneError extends Error {
+    constructor() {
+        super('Task already done')
+        this.name = 'TaskAlreadyDoneError'
+    }
+}
+
 class LoadFinishTaskRepositoryMock implements LoadFinishTaskRepository{
     taskId?: string
     callscount = 0
-    output?: TaskGroup = {
-        tasks: [
-            {
+    output?: Task = 
+            {   
                 id: 'any_id',
                 userId: 'any_user_id',
                 description: 'any_description',
                 finishedAt: new Date(),
                 title: 'any_title',
-                status: 'any_status'
+                status: 'pending'
             }
-        ]
-    }
+        
+    
 
-    async loadTask({id}: {id: string, userId: string}): Promise<TaskGroup | undefined> {
+    async loadTask({ id }: { id: string, userId: string }): Promise<Task | undefined> {
         this.taskId = id
         this.callscount++
         return this.output
@@ -83,7 +90,7 @@ describe('FinishTask', () => {
     const description = 'any_description'
     const finishedAt = new Date()
     const title = 'any_title'
-    const status = 'any_status'
+    const status: TaskStatus = 'pending'
 
     it('should get task with correct id', async () => {
         const { sut, loadFinishTaskRepository } = makeSut()
@@ -103,8 +110,7 @@ describe('FinishTask', () => {
     });
     it('should return throw if userId is invalid', async () => {
         const { sut, loadFinishTaskRepository } = makeSut()
-        loadFinishTaskRepository.output = {
-            tasks: [
+        loadFinishTaskRepository.output = 
                 {
                     id,
                     userId,
@@ -113,11 +119,19 @@ describe('FinishTask', () => {
                     title,
                     status
                 }
-            ]
-        }
     
         const promise =  sut.perform({id, userId: 'invalid_user_id'})
     
         expect(promise).rejects.toThrowError(UserIdInvalidError)
+    });
+
+    it('should throw if task is already done', async () => {
+        const { sut, loadFinishTaskRepository } = makeSut()
+        const task = loadFinishTaskRepository.output
+        if(task) task.status = 'done'
+
+        const promise = sut.perform({ id, userId })
+
+        expect(promise).rejects.toThrowError(TaskAlreadyDoneError)
     });
 });
