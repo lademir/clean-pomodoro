ba35b248118576b317d9046d5f635ce25ebb9aba
lademir <lademirjunior@hotmail.com>
2022-08-29 01:41:52 -0300

    ensure CheckLimitDate throw TasIdInvalidError if userId is invalid

--- tests/domain/usecases/check-limite-date.spec.ts
+++ tests/domain/usecases/check-limite-date.spec.ts
@@ -1,14 +1,25 @@
-import { TaskIdInvalidError } from "@/core/domain/errors";
+import { TaskIdInvalidError, UserIdInvalidError } from "@/core/domain/errors";
 import { LoadTaskRepository } from "@/core/domain/repositories";
 import { LoadTaskRepositorySpy } from "../mocks/data/load-task-mock";
+import { mockTaskModel } from "../mocks/models";
 
 class CheckLimitDate {
     constructor (private readonly loadTaskRepository: LoadTaskRepository){}
 
-    async perform({ id, userId }: LoadTaskRepository.Params): Promise<void> {
+    async perform({ id, userId }: CheckLimitDate.Params): Promise<void> {
         const task = await this.loadTaskRepository.loadTask({ id, userId })
         if(!task) throw new TaskIdInvalidError()
+        if(task.userId !== userId) throw new UserIdInvalidError()
     }
+}
+
+namespace CheckLimitDate {
+    export type Params = {
+        id: string
+        userId: string
+    }
+
+
 }
 
 type SutTypes = {
@@ -25,11 +36,16 @@ const makeSut = (): SutTypes => {
     }
 }
 
+const mockCheckLimitDateParams = (): CheckLimitDate.Params => ({
+    id: 'any_id',
+    userId: 'any_user_id'
+})
+
 describe('CheckLimitDate', () => {
     it('should get task data', async () => {
         const { sut, loadTaskRepositorySpy } = makeSut()
 
-        await sut.perform({ id: 'any_id', userId: 'any_user_id' })
+        await sut.perform(mockCheckLimitDateParams())
 
         expect(loadTaskRepositorySpy.taskId).toBe('any_id')
         expect(loadTaskRepositorySpy.userId).toBe('any_user_id')
@@ -40,8 +56,17 @@ describe('CheckLimitDate', () => {
         const { sut, loadTaskRepositorySpy } = makeSut()
         loadTaskRepositorySpy.output = undefined
 
-        const promise = sut.perform({ id: 'any_id', userId: 'any_user_id' })
+        const promise = sut.perform(mockCheckLimitDateParams())
 
         await expect(promise).rejects.toThrowError(TaskIdInvalidError)
+    });
+    
+    it('should throw if userId is invalid', async () => {
+        const { sut, loadTaskRepositorySpy } = makeSut()
+        loadTaskRepositorySpy.output = {...mockTaskModel(), userId: 'invalid_user_id'}
+
+        const promise =  sut.perform(mockCheckLimitDateParams())
+
+        await expect(promise).rejects.toThrowError(UserIdInvalidError)
     });
 });
